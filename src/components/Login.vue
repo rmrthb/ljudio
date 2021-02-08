64861a1cd3dd54b067390198bf9f83e8f2fc4d24
Michael Ernst <Michael_Ernst@hotmail.com>
2021-02-08 15:09:14 +0100

    commit to merge w main

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,22 @@
 <template>
-  
+  <div id="container">
+    <div id="modal">
+      <div id="modal-content"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+#container {
+  background-image: url("https://img.wallpapersafari.com/desktop/1680/1050/63/21/wU4tOM.jpg");
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  margin-bottom: 1vh;
+  background-position: 50% 50%;
+}
 </style>
